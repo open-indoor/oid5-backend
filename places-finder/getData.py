c8c8b6cc0c5958d5258e2dd9a7dc741ca835b725
Basile NONCLERCQ <basile@packer-output-1ffe74aa-3fcd-4dd1-84ed-6f3da813b13d>
2021-05-10 07:42:28 +0000

    init commit

--- places-finder/getData.py
+++ places-finder/getData.py
@@ -51,12 +51,13 @@ if len(os.listdir(downloadDest) ) == 0:
         for link in linksPart:
                 urlretrieve("http://download.geofabrik.de/europe/" + link, downloadDest + link[len(endDir):])
 
-# Convert all downloaded osm.pbf file to geojson file in order to fix geometry issues
+# Convert all downloaded osm.pbf file to geojson file in order to fix geometry issues and import them in the postgis database
 print(linksPart)
 convertedFiles = []
 print("Download in progress...")
 for i in range(len(linksPart)):
         process = subprocess.run(["ogr2ogr","-f","GeoJSON", downloadDest + linksPart[i][len(endDir):-7] + "geojson",downloadDest + linksPart[i][len(endDir):], "multipolygons" ])
+        # osmium extract bbox 47.394144,0.68484,47.404144,0.69484 -o output ville jean.osm.pbf http://download.geofabrik.de/europe/france/bretagne-latest.osm.pbf
         processDB = subprocess.run(["ogr2ogr","-f","PostgreSQL", "PG:dbname=openindoor-db host=openindoor-db port=5432 user=openindoor-db-admin password=admin123",downloadDest + linksPart[i][len(endDir):-7] + "geojson", "-nln", "regions" , "-overwrite", "-append", "-update", "-nlt", "MULTIPOLYGON"])
 print(convertedFiles)
 os.listdir(downloadDest)
